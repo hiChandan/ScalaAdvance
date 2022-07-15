ade9dc652e1606acac6e665d42355b7ef218a6d8
Chandan Chauhan <chandan.chauhan@ril.com>
2022-07-15 11:40:32 +0530

    Variance use cases

---
+++ src/main/scala/VarianceExample.scala
@@ -0,0 +1,18 @@
+package src.main.scala
+
+object VarianceExample extends App {
+  trait Animal
+  class Cat extends Animal
+  class Dog extends Animal
+// Useful in generic collection
+//  class Invariance[A] Invariance
+//  class Invariance[-A] Contravariance
+  class Invariance[+A] // co variance
+  def doSomething(list: Invariance[Animal]) = {
+    println("I am doiing something")
+  }
+  //  val animal = new Animal()
+  doSomething(new Invariance[Dog]) // co -variance
+  doSomething(new Invariance[Cat]) // contra -variance
+  doSomething(new Invariance[Animal]) // variance
+}
