a8d86537cab0d8af5782ee9e1e856c7156146566
Chandan Chauhan <chandan.chauhan@ril.com>
2022-07-17 11:38:30 +0530

    How to create generic library methods

---
+++ src/main/scala/LibraryMethods.scala
@@ -0,0 +1,27 @@
+package src.main.scala
+
+object LibraryMethods {
+
+  trait SummableTrait[T] {
+    def sumIt(l: List[T]): T
+  }
+
+  implicit object SumInt extends SummableTrait[Int] {
+    override def sumIt(l: List[Int]): Int = l.sum
+  }
+
+  implicit object SumString extends SummableTrait[String] {
+    override def sumIt(l: List[String]): String = l.mkString("")
+  }
+
+  def processData[T](l: List[T])(implicit summable: SummableTrait[T]): T = {
+    summable.sumIt(l)
+  }
+
+  def main(args: Array[String]): Unit = {
+    print(processData(List(1, 3, 4, 5)))
+    print(processData(List("Hi", " World")))
+    //print(processData(List(true,false))) //Not possible
+  }
+
+}
