b6d7fbfbc0698deb65f763f97bc5adb65e7d0da3
Chandan Chauhan <chandan.chauhan@ril.com>
2022-07-29 12:43:47 +0530

    Type example

--- src/main/scala/TypeExample.scala
+++ src/main/scala/TypeExample.scala
@@ -1,16 +1,19 @@
 package src.main.scala
 
 object TypeExample extends App {
-
+  //Type declaration is a Scala feature that enables us to declare our own types.
+  // Use for semantic only
   type MovieRating = (Int, Double)
   type UserRatingPair = (Int, (MovieRating))
-  def makePairs(userRatings: UserRatingPair): ((Int, Double)) = {
+  // Check use of type in below statement
+//  Earlier def makePairs(userRatings: UserRatingPair):((Int, Double),(Int, Double))
+  def makePairs(userRatings: UserRatingPair): (MovieRating, MovieRating) = {
     val movieRating1 = userRatings._2
 
     val movie1 = movieRating1._1
     val rating1 = movieRating1._2
 
-    (movie1, rating1)
+    ((movie1, rating1), (movie1, rating1))
   }
 
 }
