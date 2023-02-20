26cd040c2ad5617ea13b2b633d15a9763b920a03
Chandan Chauhan <chandan.chauhan@ril.com>
2023-02-20 12:21:43 +0530

    Multithreading in scala

--- src/main/scala/mutithreading/ThreadFutureAllComplete.scala
+++ src/main/scala/mutithreading/ThreadFutureAllComplete.scala
@@ -2,31 +2,50 @@ package mutithreading
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.concurrent.duration.NANOSECONDS
 import scala.io.Source
+import scala.util.{Failure, Success}
 
 object ThreadFutureAllComplete extends App {
-  def getData(s: String) = {
+  def getData(s: String, i: Int) = {
     val f1 = Source.fromURL(s)
     val res = s + " " + f1.take(10).toList
     f1.close()
-    res
+    var d = 0
+    for (a <- 1 to i) {
+      d += a
+    }
+
+    d
   }
 
-  val f1 = Future(getData("https://www.google.com"))
-  val f2 = Future(getData("https://www.yahoo.com"))
-  val f3 = Future(getData("https://www.bing.com"))
+  import java.lang.management.ManagementFactory
+
+  def runAndTime(f: => Unit) = {
+    val start = ManagementFactory.getThreadMXBean.getCurrentThreadCpuTime
+    f
+    NANOSECONDS.toMillis(
+      ManagementFactory.getThreadMXBean.getCurrentThreadCpuTime - start
+    )
+  }
+  val f1 = Future(runAndTime(getData("https://www.google.com", 20000)))
+  val f2 = Future(runAndTime(getData("https://www.yahoo.com", 100)))
+  val f3 = Future(runAndTime(getData("https://www.bing.com", 2000)))
 
   val af = Seq(f1, f2, f3)
   val all = Future.sequence(af)
-//  all.onComplete({
-//    case Success(n)  => println(n)
-//    case Failure(ex) => println("Failed with exception" + ex.getMessage)
-//  })
-  Future.firstCompletedOf(af).foreach(a => println(a))
-  Thread.sleep(3000)
-  println(all.isCompleted)
-  Thread.sleep(3000)
+  all.onComplete({
+    case Success(n)  => println(n)
+    case Failure(ex) => println("Failed with exception" + ex.getMessage)
+  })
+//  Future.firstCompletedOf(af).foreach(a => println(a))
+//  Thread.sleep(3000)
+//  println(all.isCompleted)
+  Thread.sleep(10000)
 //  println(getData("https://www.yahoo.com"))
+
+  //Demo 3
+
 //  readLine()
 
 }
